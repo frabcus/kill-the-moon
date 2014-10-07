0b5cc9215170176613d3f4532a97db9352db6329
Francis Irving <francis@flourish.org>
2014-10-07 23:05:16 +0100

    Works a bit

---
+++ README.md
@@ -0,0 +1,35 @@
+Kill the Moon Electricity Use
+=============================
+
+
+
+Broadcast times
+---------------
+
+Broadcast time: 20:29:10 (http://twidw.doctorwhonews.net/listing.php?bdid=52388)
+35:00 "night night" all the lights go out
+
+Which is to say at: 21:04:10
+
+
+Previous Doctor Who episode start times:
+
+2014-10-04 20:29:10
+2014-09-27 20:30:00
+2014-09-20 19:30:00
+2014-09-13 19:30:00
+2014-09-06 19:30:00
+2014-08-30 19:30:00
+2014-08-23 19:50:00
+
+
+
+Electricity data
+----------------
+
+Downloaded from GridWatch database
+
+http://www.gridwatch.templar.co.uk/download.php
+
+
+
---
+++ plot.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+# Doctor Who, "Kill the Moon" electricity use
+
+import pandas
+import datetime
+import numpy
+import sys
+import dateutil.parser
+
+from ggplot import *
+import pandas as pd
+
+def do_date(x):
+    d = dateutil.parser.parse(x)
+    d = d.replace(year = 1900, month = 1, day = 1)
+    return numpy.datetime64(d)
+
+def read_file(name):
+    df = pandas.io.parsers.read_csv(name, parse_dates = [2], skipinitialspace = True, date_parser = do_date)
+    return df
+
+#dfs = []
+#df = read_file("2014-10-04.csv")
+#df.append(read_file("2014-09-27.csv"))
+#df.append(read_file("2014-09-20.csv"))
+#df.append(read_file("2014-09-13.csv"))
+#df.append(read_file("2014-09-06.csv"))
+#df.append(read_file("2014-08-30.csv"))
+#df.append(read_file("2014-08-23.csv"))
+
+df = read_file("all.csv")
+
+print(df.head())
+print(df.describe())
+print(df.dtypes)
+
+p = ggplot(aes(x='timestamp', y='demand', color='datestamp'), df) + geom_line() #+ scale_x_date(labels = date_format("%Y-%m-%d %H:%M"), breaks='1 hour')
+
+#for df in dfs[1:]:
+#    p = p + geom_line(aes(x='timestamp', y='demand'), df, color='blue')
+
+p = p + xlab("Time")
+p = p + ylab("UK electricity demand (MW)")
+p = p + ggtitle('''Electricity demand during Doctor Who episodes, comparing with "Kill the Moon"\n''')
+
+p = p + scale_x_date(labels = date_format("%H:%M"), breaks='1 hour')
+
+ggsave(p, "foo.png")
+
+
+
